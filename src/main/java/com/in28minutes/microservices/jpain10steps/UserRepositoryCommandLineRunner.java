e6d39cc69fc40fd348355582a5763ac9cd9e94a9
mateusfernandescosta <mateusfernandes.costa@gmail.com>
2018-10-06 22:58:14 -0300

    Find One and All Users by Command line Runner

--- src/main/java/com/in28minutes/microservices/jpain10steps/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/in28minutes/microservices/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.jpain10steps;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -20,9 +23,15 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// New User is created : User(id=2, name=Jill, role=Admin)
+		
 		User user = new User("Jill", "Admin");
 		repository.save(user);
 		log.info("New User is created : " + user);
+		
+		Optional<User> userWithIdOne = repository.findById(1L);
+		log.info("User is retrived : " + userWithIdOne);
+		
+		List<User> users = repository.findAll();
+		log.info("All Users : " + users);
 	}
 }
